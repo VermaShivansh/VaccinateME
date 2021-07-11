7a088340fb8fee3aef29a71925d5369334f923a8
ShivanshVerma-coder <Shivansh29.sv@gmail.com>
2021-07-12 04:30:47 +0530

    new

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
+import Axios from "axios"
+Axios.defaults.baseURL = "https://vaccinate-me-backend.herokuapp.com/"
 
 ReactDOM.render(<App />, document.getElementById("root"))
 
