50728b60c9f0f510f234e3fd922706ff1f21f73a
ShivanshVerma-coder <Shivansh29.sv@gmail.com>
2021-07-12 03:30:02 +0530

    make it better

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
 import Axios from "axios"
-Axios.defaults.baseURL = "http://localhost:8080"
+Axios.defaults.baseURL = process.env.baseURL
 
 ReactDOM.render(<App />, document.getElementById("root"))
 
