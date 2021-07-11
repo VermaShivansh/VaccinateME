0134f2478df18f17dd73bb3557044ac38012352c
ShivanshVerma-coder <Shivansh29.sv@gmail.com>
2021-07-12 03:46:12 +0530

    new commit

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
-import Axios from "axios"
-Axios.defaults.baseURL = process.env.baseURL
 
 ReactDOM.render(<App />, document.getElementById("root"))
 
