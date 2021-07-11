0515fb6d0c99e64fe6b3358129c18315c20f5316
ShivanshVerma-coder <Shivansh29.sv@gmail.com>
2021-07-12 04:33:16 +0530

    new

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(express.json())
 
 // Add Access Control Allow Origin headers
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://dtd-vaccination.herokuapp.com/")
+  res.setHeader("Access-Control-Allow-Origin", "https://dtd-vaccination.herokuapp.com")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   next()
 })
