ff167a5f42accf38ee6014cd44f72c958063a2a6
ShivanshVerma-coder <Shivansh29.sv@gmail.com>
2021-07-13 03:43:45 +0530

    added Flash messages, UI Improved

--- src/components/Header.js
+++ src/components/Header.js
@@ -27,19 +27,19 @@ function Header() {
   return (
     <div className="header">
       <div className="headerdesign">
-        <div className="maincontent py-5 px-4 mx-3 mx-sm-4 my-5 p-md-5 m-md-5">
+        <div className="maincontent py-5 px-4 mx-3 mx-sm-4 my-5 p-md-5 m-md-5 position-relative">
           <div className="heading display-1 mb-5">Door-To-Door Vaccination</div>
-          <div className="tagline mt-md-4 row position-relative">
-            <div className="container taglineContent p-0 m-3">
+          <div className="tagline mt-md-4 row">
+            <div className="container taglineContent p-0 m-3" style={{ zIndex: 10 }}>
               <h3>Easy Vaccination in just 3 steps : -</h3>
               <li>1. Register with your Mobile Number.</li>
               <li>2. Track your Vaccination Status through CustomerID.</li>
               <li>3. Verify your Vaccination and download Certificate.</li>
               <li>Happy Vaccination!!</li>
             </div>
-            <div className="col-6 position-absolute imageContainer">
-              <img src="https://i.ibb.co/rykDD3G/Pngtree-vaccine-doctor-inject-vaccines-png-6319646.png" alt="Pngtree-vaccine-doctor-inject-vaccines-png-6319646" border="0" width="600" />
-            </div>
+          </div>
+          <div className="col-6 position-absolute imageContainer">
+            <img src="https://i.ibb.co/rykDD3G/Pngtree-vaccine-doctor-inject-vaccines-png-6319646.png" alt="Pngtree-vaccine-doctor-inject-vaccines-png-6319646" border="0" width="600" />
           </div>
         </div>
       </div>
--- src/components/StaffDetails.js
+++ src/components/StaffDetails.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react"
-import { Accordion, Container, Table, Button, Modal, Form } from "react-bootstrap"
+import { Accordion, Container, Table, Button, Modal, Form, Alert } from "react-bootstrap"
 import Axios from "axios"
 function StaffDetails(props) {
   let [data, setdata] = useState([])
   let [modalShow, setModalShow] = React.useState(false)
+  const [show, setShow] = useState(-1)
   useEffect(() => {
     getData()
   }, [])
@@ -22,9 +23,40 @@ function StaffDetails(props) {
     try {
       // console.log(customerId, authCode)
       let response = await Axios.post("login/vendor/vaccinated", { customerId, authCode })
-      // console.log(response)
+      setModalShow(false)
+      if (response.status == 200) {
+        setShow(1)
+        document.querySelector(".alertingBox").classList.add("opacity1")
+        setTimeout(() => {
+          document.querySelector(".alertingBox").classList.remove("opacity1")
+        }, 5000)
+        setTimeout(() => {
+          setShow(-1)
+        }, 6000)
+        window.scrollTo(0, 0)
+      } else {
+        setShow(0)
+        document.querySelector(".alertingBox").classList.add("opacity1")
+        setTimeout(() => {
+          document.querySelector(".alertingBox").classList.remove("opacity1")
+        }, 5000)
+        setTimeout(() => {
+          setShow(-1)
+        }, 6000)
+        window.scrollTo(0, 0)
+      }
     } catch (e) {
+      setModalShow(false)
       console.log(e)
+      setShow(0)
+      document.querySelector(".alertingBox").classList.add("opacity1")
+      setTimeout(() => {
+        document.querySelector(".alertingBox").classList.remove("opacity1")
+      }, 5000)
+      setTimeout(() => {
+        setShow(-1)
+      }, 6000)
+      window.scrollTo(0, 0)
     }
   }
 
@@ -49,7 +81,7 @@ function StaffDetails(props) {
               <Form.Control onChange={(e) => (authCode = e.target.value)} autoComplete="off" type="text" placeholder="Auth Code" maxLength="6" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="I have Verified the Vaccination" />
+              <Form.Check required type="checkbox" label="I have Verified the Vaccination" />
             </Form.Group>
             <hr />
             <Button variant="primary" type="submit">
@@ -63,6 +95,20 @@ function StaffDetails(props) {
 
   return (
     <div className="midbody staff p-0 ">
+      <div className="text-center col-12 alertingBox p-1">
+        {show != -1 && (
+          <Alert variant={show ? "success" : "danger"} className="col-md-5 m-auto " onClose={() => setShow(-1)} dismissible>
+            <Alert.Heading>
+              {" "}
+              {show == 0 && <span>Failed!!!</span>} {show == 1 && <span> Vaccination Successfully Verified!!!</span>}
+            </Alert.Heading>
+            <hr />
+            <p>
+              {show == 0 && <span>CustomerID and Auth Code do not Match !</span>} {show == 1 && <span> User has been successfully verified.</span>}{" "}
+            </p>
+          </Alert>
+        )}
+      </div>
       <Container className="offset-md-1 mb-3 col-md-10 p-0 p-md-2">
         <Button variant="light" onClick={() => setModalShow(true)}>
           Verify A Vaccination
